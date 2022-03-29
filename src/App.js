2d1dd9b7979f6bfb50b38f19d50d5554b29f5077
lebrasr1 <96599337+lebrasr1@users.noreply.github.com>
2022-03-29 17:32:03 +0200

    First class hierarchy for Questions

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
 import "./styles.css";
 
+class Question {
+  constructor() {}
+  render() {
+    return "";
+  }
+}
+
+class QCMHistoire extends Question {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return "Hello world";
+  }
+}
+
 export default function App() {
   function getData(val) {
     console.warn(val.target.value);
@@ -7,6 +24,7 @@ export default function App() {
   function handleClick() {
     console.warn("click !");
   }
+  q = new QCMHistoire();
   return (
     <div className="App">
       <h1>Mathématiques</h1>
@@ -14,6 +32,7 @@ export default function App() {
       1+1=
       <input type="text" onChange={getData} />
       <button onClick={handleClick}> Click me !</button>
+      {q.render()}
     </div>
   );
 }
