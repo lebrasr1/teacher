6683e17fd6b27853a5e111273d5f8b6e42b2129e
lebrasr1 <96599337+lebrasr1@users.noreply.github.com>
2022-03-29 15:20:06 +0200

    learn to do input and button in React

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import "./styles.css";
 
 export default function App() {
+  function getData(val) {
+    console.warn(val.target.value);
+  }
+  function handleClick() {
+    console.warn("click !");
+  }
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <h1>Mathématiques</h1>
       <h2>Start editing to see some magic happen!</h2>
+      1+1=
+      <input type="text" onChange={getData} />
+      <button onClick={handleClick}> Click me !</button>
     </div>
   );
 }
